cc85aefa2328b98ca8f31dfc3ff0bcf0a67d366a
Trung Hiếu Trương <kimthu86zz@gmail.com>
2024-09-04 11:36:35 +0700

    Add project files.

---
+++ SonaBookings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SonaBookings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
